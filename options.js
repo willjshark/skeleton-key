9d61c367a510b8022b7499b8dde168e1c18c61fc
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-01-13 11:21:03 +0000

    iterate through keys

--- options.js
+++ options.js
@@ -9,36 +9,32 @@ const presetLinks = [
 
 // Reacts to a button click by marking the selected button and saving
 // the selection
-function handleButtonClick(event) {
-  // Remove styling from the previously selected color
-  let current = event.target.parentElement.querySelector(
-    `.${selectedClassName}`
-  );
-  if (current && current !== event.target) {
-    current.classList.remove(selectedClassName);
-  }
+// function handleButtonClick(event) {
+//   // Remove styling from the previously selected color
+//   let current = event.target.parentElement.querySelector(
+//     `.${selectedClassName}`
+//   );
+//   if (current && current !== event.target) {
+//     current.classList.remove(selectedClassName);
+//   }
 
-  // Mark the button as selected
-  let color = event.target.dataset.color;
-  event.target.classList.add(selectedClassName);
-  chrome.storage.sync.set({ color });
-}
+//   // Mark the button as selected
+//   let color = event.target.dataset.color;
+//   event.target.classList.add(selectedClassName);
+//   chrome.storage.sync.set({ color });
+// }
 
 // Add a button to the page for each supplied color
-function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
-    let currentColor = data.color;
+function constructOptions {
+  chrome.storage.sync.get("keys", (data) => {
+    let currentKeys = data.keys;
+    console.log(currentKeys)
     // For each color we were provided…
-    for (let buttonColor of buttonColors) {
+    for (let key in currentKeys) {
       // …create a button with that color…
       let button = document.createElement("button");
-      button.dataset.color = buttonColor;
-      button.style.backgroundColor = buttonColor;
-
-      // …mark the currently selected color…
-      if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
-      }
+      button.innerText = key[0]
+      console.log(key)
 
       // …and register a listener for when that button is clicked
       button.addEventListener("click", handleButtonClick);
@@ -48,4 +44,4 @@ function constructOptions(buttonColors) {
 }
 
 // Initialize the page by constructing the color options
-constructOptions(presetLinks);
+constructOptions();
