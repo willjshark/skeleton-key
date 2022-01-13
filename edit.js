3c073cb7fdcea8e2202f373828d6e09c01b1edcc
willjshark <willjshark@gmial.com>
2022-01-13 12:04:40 +0000

    Can add new bits to social key

--- edit.js
+++ edit.js
@@ -1,19 +1,32 @@
-let button_press = document.getElementById("btn");
 
-button_press.addEventListener("click", addingUrl);
+let workKeyEdit = document.getElementById("workKeyEdit");
+let socialKeyEdit = document.getElementById("socialKeyEdit");
+
+workKeyEdit.addEventListener("click", addingUrl);
+socialKeyEdit.addEventListener("click", addingUrl);
 
 function addingUrl(ev) {
 ev.preventDefault();
-chrome.storage.sync.get("workLinks", ({ workLinks }) => {
-    let input_url = document.getElementById("new_url").value;
 
+let id = ev.target.id;
+  if (id == "workKeyEdit") {
+    chrome.storage.sync.get("workLinks", ({ workLinks }) => {
+    let input_url = document.getElementById("new_url").value;
     workLinks.push(input_url);
-    
-   chrome.storage.sync.set({workLinks});
-   document.forms[0].reset();
-  });
+    chrome.storage.sync.set({workLinks});
+    document.forms[0].reset();
+});
+} else if (id == "socialKeyEdit"){
+    chrome.storage.sync.get("socialLinks", ({ socialLinks }) => {
+    let input_url = document.getElementById("new_url").value;
+    socialLinks.push(input_url);
+    chrome.storage.sync.set({socialLinks});
+    document.forms[0].reset();
+
+});
+  
 }
 
 
 
- 
+}
