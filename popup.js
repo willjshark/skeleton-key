0a95c533a77966179c0431d2f56f55c0add6267e
Jacob <jacobchanrahan@gmail.com>
2022-01-14 10:22:55 +0000

    Adding keys to Popup

--- popup.js
+++ popup.js
@@ -1,8 +1,31 @@
-let workKey = document.getElementById("workKey");
-let socialKey = document.getElementById("socialKey");
+// import { constructOptions } from './modules/constructOptions.js';
 
-workKey.addEventListener("click", openLinks);
-socialKey.addEventListener("click", openLinks);
+let page = document.getElementById("buttonDiv");
+// let workKey = document.getElementById("workKey");
+// let socialKey = document.getElementById("socialKey");
+
+// workKey.addEventListener("click", openLinks);
+// socialKey.addEventListener("click", openLinks);
+
+function constructOptions(page) {
+  chrome.storage.sync.get("keys", (data) => {
+    let currentKeys = data.keys;
+    console.log(currentKeys);
+    // For each color we were provided…
+    for (let key in currentKeys) {
+      // …create a button with that color…
+      let button = document.createElement("button");
+      button.innerText = key;
+      button.id = key;
+      console.log(key);
+      
+
+      button.addEventListener("click", openLinks);
+
+      page.appendChild(button);
+    }
+  });
+}
 
 function openLinks(e) {
     chrome.storage.sync.get("keys", ({ keys }) => {
@@ -19,3 +42,5 @@ function createTabs(links) {
     chrome.tabs.create({ url: link });
   });
 }
+
+constructOptions(page)
