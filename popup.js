972b02f3002c2b4915a6b86ee1e0ae818cc52413
roshanabey2 <roshanabey2@gmail.com>
2022-01-19 14:12:18 +0000

    a lil bit more refactoring

--- popup.js
+++ popup.js
@@ -1,19 +1,12 @@
 // import { constructOptions } from './modules/constructOptions.js';
 
 let page = document.getElementById("buttonDiv");
-// let workKey = document.getElementById("workKey");
-// let socialKey = document.getElementById("socialKey");
-
-// workKey.addEventListener("click", openLinks);
-// socialKey.addEventListener("click", openLinks);
 
 function constructOptions(page) {
   chrome.storage.sync.get("keys", (data) => {
     let currentKeys = data.keys;
     console.log(currentKeys);
-    // For each color we were provided…
     for (let key in currentKeys) {
-      // …create a button with that color…
       let button = document.createElement("button");
       button.innerText = key;
       button.id = key;
