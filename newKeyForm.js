f0711cb2f816b6d376088bf46711c577a5056666
Jacob <jacobchanrahan@gmail.com>
2022-01-19 15:17:29 +0000

    Key rules

--- newKeyForm.js
+++ newKeyForm.js
@@ -7,12 +7,16 @@ function addKey(e) {
   e.preventDefault();
 
   let name = newKeyname.value;
-  console.log(name);
-
-  chrome.storage.sync.get("keys", ({ keys }) => {
+  chrome.storage.sync.get("keys", ( {keys}) => {
+  if (name == "") 
+  { window.alert("Key Must Not Be Blank!")
+  }else if (name.length > 7)
+  { window.alert("Key Name Can Not Be More Than 7 Characters!")
+  }else if (name in keys) {window.alert("Key Already Exists!")
+  }else { 
     keys[name] = [];
     chrome.storage.sync.set({ keys });
     document.forms[0].reset();
-  });
+  
   window.location.href = "./options.html"
-}
+}})}
