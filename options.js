a1a4f0accdf2614403b4e750e667cd2a6e0732a7
roshanabey2 <roshanabey2@gmail.com>
2022-01-19 12:15:51 +0000

    got rid go some pointless code

--- options.js
+++ options.js
@@ -1,23 +1,5 @@
 import { constructOptions } from "./modules/constructOptions.js";
 let page = document.getElementById("buttonDiv");
-function openLinks(e) {
-  console.log(e.target.innerText);
-  let keyName = e.target.innerText;
-  if (keyName == "work") {
-    chrome.storage.sync.get("workLinks", ({ workLinks }) => {
-      createTabs(workLinks);
-    });
-  } else if (keyName == "social") {
-    chrome.storage.sync.get("socialLinks", ({ socialLinks }) => {
-      createTabs(socialLinks);
-    });
-  }
-}
-function createTabs(links) {
-  links.forEach((link) => {
-    chrome.tabs.create({ url: link });
-  });
-}
 function openEdit(e) {
   let currentKey = e.target.innerText;
   chrome.storage.sync.set({ currentKey });
@@ -26,11 +8,8 @@ function openEdit(e) {
 // Initialize the page by constructing the color options
 constructOptions(page);
 
-/* <a href="todolist.html">
-  {" "}
-  <input type="button" value="ToDo list">
-    To do list
-  </input>{" "}
-</a>; */
+
 
 export { openEdit };
+
+
